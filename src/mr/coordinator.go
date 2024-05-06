60c76857158e43b70a0923d4af8d31555b42d382
“BlackGhostLzc” <2427324519@qq.com>
2024-05-06 23:20:16 +0800

    lab1 is finished

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -6,28 +6,189 @@ import "os"
 import "net/rpc"
 import "net/http"
 
+// import "fmt"
+import "sync"
+import "time"
+import "strconv"
+
+const (
+	Map_phase    = iota // 0
+	Reduce_phase        // 1
+	Done_phase          // 2
+)
+
+const (
+	Map_task     = iota // 0
+	Reduce_task         // 1
+	Waiting_task        // 2
+)
+
+// 任务，无论是MapJob还是ReduceJob都是这个结构体
+type Job struct {
+	Jobtype  int    // Waiting , Map, Reduce,
+	Filename string // MapJob才会需要
+	Jobid    int    // MapJob需要用到  创建nreduce 个 mr-out-Jobid-X 文件作为记录
+	NReduce  int
+	Filenum  int
+}
+
+type JobInfo struct {
+	StartTime time.Time
+	JobPtr    *Job
+}
 
 type Coordinator struct {
 	// Your definitions here.
+	Current_phase      int
+	Reduce_num         int
+	Maptask_queue      chan *Job // 虽然说chan不可传递指针，但我们只是把chan当做一个队列使用，不会有其他线程从chan中取数据
+	Reducetask_queue   chan *Job
+	Mapblock_record    map[int]*JobInfo // map 数据结构
+	Reduceblock_record map[int]*JobInfo // map 数据结构
+
+	Mapblock_mutex    sync.Mutex // 专门来锁 Mapblock_record这个数据结构
+	Reduceblock_mutex sync.Mutex
+}
+
+func (c *Coordinator) Initializing(files []string, nReduce int) {
+	c.Current_phase = Map_phase
+	c.Reduce_num = nReduce
+	c.Maptask_queue = make(chan *Job, 20)
+	c.Reducetask_queue = make(chan *Job, 20)
+
+	c.Mapblock_record = make(map[int]*JobInfo)
+	c.Reduceblock_record = make(map[int]*JobInfo)
+
+	for i := 0; i < len(files); i++ {
+		job := Job{
+			Jobtype:  Map_task,
+			Filename: files[i],
+			Jobid:    i + 1,
+			NReduce:  nReduce,
+			Filenum:  len(files),
+		}
+		// fmt.Printf("Add map job to maptask_queue, filename is [%s]\n", job.Filename)
+		c.Maptask_queue <- &job
+	}
+
+	for i := 0; i < nReduce; i++ {
+		job := Job{
+			Jobtype: Reduce_task,
+			Jobid:   i,
+			NReduce: nReduce,
+			Filenum: len(files),
+		}
+		// fmt.Printf("Add reduce job to reducetask_queue\n")
+		c.Reducetask_queue <- &job
+	}
 
 }
 
 // Your code here -- RPC handlers for the worker to call.
 
-//
 // an example RPC handler.
 //
 // the RPC argument and reply types are defined in rpc.go.
-//
 func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
 	reply.Y = args.X + 1
 	return nil
 }
 
+func (c *Coordinator) ReceiveFinishMessage(job *Job, args *ExampleArgs) error {
+	if job.Jobtype == Map_task {
+		// fmt.Printf("Receive finish a Map_task message, Jobid is %v\n", job.Jobid)
+		// 阻塞队列清除，还要考虑是不是超出了时间限制，上锁？
+		c.Mapblock_mutex.Lock()
+		defer c.Mapblock_mutex.Unlock()
 
-//
+		t2 := time.Now()
+		t1 := c.Mapblock_record[job.Jobid].StartTime
+		seconds := t2.Sub(t1).Seconds()
+		// fmt.Printf("This [map] Job finish time is %v seconds\n", seconds)
+
+		if seconds > 10 {
+			// 把job再放入Maptask_queue中,不应该在这里做，而应该开一个协程来处理
+			// c.Maptask_queue <- Mapblock_record[job.Jobid].JobPtr
+			// fmt.Printf("Time exceeded!\n")
+		} else {
+			// 把任务从阻塞队列中删除
+			delete(c.Mapblock_record, job.Jobid)
+			// fmt.Printf("Delete a [map] task Successfully\n")
+		}
+
+	} else if job.Jobtype == Reduce_task {
+		// fmt.Printf("Receive finish a Reduce_task message, Jobid is %v\n", job.Jobid)
+		// 阻塞队列清除，考虑是否超出时间限制
+		c.Reduceblock_mutex.Lock()
+		defer c.Reduceblock_mutex.Unlock()
+
+		t2 := time.Now()
+		t1 := c.Reduceblock_record[job.Jobid].StartTime
+		seconds := t2.Sub(t1).Seconds()
+		// fmt.Printf("This [reduce] Job finish time is %v seconds\n", seconds)
+
+		if seconds > 10 {
+			// fmt.Printf("Time exceeded!\n")
+		} else {
+			// 把任务从阻塞队列中删除
+			delete(c.Reduceblock_record, job.Jobid)
+
+			// 再把中间文件删除
+			// mr-out-X-job.Jobid
+			for i := 1; i <= job.Filenum; i++ {
+				filename := "mr-out-" + strconv.Itoa(i) + "-" + strconv.Itoa(job.Jobid)
+				os.Remove(filename)
+			}
+			// fmt.Printf("Delete a [reduce] task Successfully\n")
+		}
+
+	}
+	return nil
+}
+
+func (c *Coordinator) DistributeTask(args *ExampleArgs, reply *Job) error {
+	if c.Current_phase == Map_phase {
+		if len(c.Maptask_queue) == 0 {
+			reply.Jobtype = Waiting_task
+		} else {
+			tmp := <-c.Maptask_queue
+			// 加入阻塞队列
+			c.Mapblock_mutex.Lock()
+
+			jobinfo := JobInfo{
+				StartTime: time.Now(),
+				JobPtr:    tmp,
+			}
+			c.Mapblock_record[tmp.Jobid] = &jobinfo
+			// fmt.Printf("Adding a Map[jobid:%d] job to Mapblock_record\n", tmp.Jobid)
+
+			*reply = *tmp
+			c.Mapblock_mutex.Unlock()
+		}
+	} else if c.Current_phase == Reduce_phase {
+		if len(c.Reducetask_queue) == 0 {
+			reply.Jobtype = Waiting_task
+		} else {
+			tmp := <-c.Reducetask_queue
+			// 加入阻塞队列
+			c.Reduceblock_mutex.Lock()
+
+			jobinfo := JobInfo{
+				StartTime: time.Now(),
+				JobPtr:    tmp,
+			}
+			c.Reduceblock_record[tmp.Jobid] = &jobinfo
+			// fmt.Printf("Adding a Reduce[jobid:%d] job to Mapblock_record\n", tmp.Jobid)
+
+			*reply = *tmp
+			c.Reduceblock_mutex.Unlock()
+		}
+
+	}
+	return nil
+}
+
 // start a thread that listens for RPCs from worker.go
-//
 func (c *Coordinator) server() {
 	rpc.Register(c)
 	rpc.HandleHTTP()
@@ -41,29 +202,88 @@ func (c *Coordinator) server() {
 	go http.Serve(l, nil)
 }
 
-//
 // main/mrcoordinator.go calls Done() periodically to find out
 // if the entire job has finished.
-//
 func (c *Coordinator) Done() bool {
 	ret := false
 
-	// Your code here.
-
+	if c.Current_phase == Done_phase {
+		ret = true
+	}
 
 	return ret
 }
 
-//
 // create a Coordinator.
 // main/mrcoordinator.go calls this function.
 // nReduce is the number of reduce tasks to use.
-//
+func (c *Coordinator) CheckTimeoutTasks() {
+	for {
+
+		if c.Current_phase == Map_phase {
+			// fmt.Printf("Current phase is Map_phase\n")
+
+			c.Mapblock_mutex.Lock()
+
+			if len(c.Maptask_queue) == 0 && len(c.Mapblock_record) == 0 {
+				c.Current_phase = Reduce_phase
+				continue
+			}
+
+			// 对于阻塞队列中的所有任务判断时间
+			t2 := time.Now()
+			for k, v := range c.Mapblock_record {
+				// k->jobid        v->*JobInfo
+				t1 := v.StartTime
+				seconds := t2.Sub(t1).Seconds()
+				if seconds > 10 {
+					// fmt.Printf("CheckTimeoutTasks find a Map task time exceeded!\n")
+					c.Maptask_queue <- v.JobPtr
+					delete(c.Mapblock_record, k)
+				}
+			}
+
+			c.Mapblock_mutex.Unlock()
+		} else if c.Current_phase == Reduce_phase {
+			// fmt.Printf("Current phase is Reduce_phase \n")
+
+			c.Reduceblock_mutex.Lock()
+
+			if len(c.Reducetask_queue) == 0 && len(c.Reduceblock_record) == 0 {
+				c.Current_phase = Done_phase
+				// Exit(0)
+			}
+
+			t2 := time.Now()
+			for k, v := range c.Reduceblock_record {
+				// k->jobid        v->*JobInfo
+				t1 := v.StartTime
+				seconds := t2.Sub(t1).Seconds()
+				if seconds > 10 {
+					// fmt.Printf("CheckTimeoutTasks find a Map task time exceeded!\n")
+					c.Reducetask_queue <- v.JobPtr
+					delete(c.Reduceblock_record, k)
+				}
+			}
+
+			c.Reduceblock_mutex.Unlock()
+		} else {
+			// fmt.Printf("I am CheckTimeoutTasks, my task is finished\n")
+			time.Sleep(2 * time.Second)
+		}
+
+		time.Sleep(2 * time.Second)
+	}
+}
+
 func MakeCoordinator(files []string, nReduce int) *Coordinator {
 	c := Coordinator{}
 
-	// Your code here.
+	// 接着要初始化 coordinator
+	// fmt.Printf("Initializing Coordinator...\n")
+	c.Initializing(files, nReduce)
 
+	go c.CheckTimeoutTasks()
 
 	c.server()
 	return &c
--- src/mr/worker.go
+++ src/mr/worker.go
@@ -4,45 +4,217 @@ import "fmt"
 import "log"
 import "net/rpc"
 import "hash/fnv"
-
+import "time"
+import "os"
+import "io/ioutil"
+import "strconv"
+import "encoding/json"
+import "sort"
 
 //
 // Map functions return a slice of KeyValue.
 //
+
 type KeyValue struct {
 	Key   string
 	Value string
 }
 
-//
 // use ihash(key) % NReduce to choose the reduce
 // task number for each KeyValue emitted by Map.
-//
 func ihash(key string) int {
 	h := fnv.New32a()
 	h.Write([]byte(key))
 	return int(h.Sum32() & 0x7fffffff)
 }
 
-
 //
 // main/mrworker.go calls this function.
 //
-func Worker(mapf func(string, string) []KeyValue,
-	reducef func(string, []string) string) {
+
+func Worker(mapf func(string, string) []KeyValue, reducef func(string, []string) string) {
 
 	// Your worker implementation here.
-
+	// fmt.Printf("Woker inititialized ....\n")
 	// uncomment to send the Example RPC to the coordinator.
+	// WorkerJob = new(Job)
 	// CallExample()
+
+	for {
+
+		// 得到 Task
+		job := Job{}
+		args := ExampleArgs{} // 这个参数没有用，返回的在 task中
+		ok := call("Coordinator.DistributeTask", &args, &job)
+		if !ok {
+			log.Fatalf("DistributeTask failed")
+		}
+
+		if job.Jobtype == Waiting_task {
+			// 休眠 1 秒钟
+			// fmt.Printf("The task has been distributed but some are not finished\n")
+			time.Sleep(1 * time.Second)
+			continue
+		}
+		// 做完任务一直取
+		// GetTaskFromCoordinator()
+		// 执行任务
+		ProcessTask(&job, mapf, reducef)
+
+	}
 
 }
 
-//
+var JobtypeToJobname = map[int]string{
+	0: "MapTask",
+	1: "ReduceTask",
+	2: "WaitingTask",
+}
+
+func ProcessTask(job *Job, mapf func(string, string) []KeyValue, reducef func(string, []string) string) {
+	// fmt.Printf("This is a [%s] task, Jobid is [%d].\n", JobtypeToJobname[job.Jobtype], job.Jobid)
+	if job.Jobtype == Map_task {
+		ProcessMapTask(job, mapf)
+		// 做完之后发送一条信息给coordinator,希望把它阻塞队列中的Map任务给抹掉
+		// SendFinishMessageToCoordinator()
+		SendFinishMessageToCoordinator(job)
+	} else if job.Jobtype == Reduce_task {
+		ProcessReduceTask(job, reducef)
+		// 做完之后发送一条信息给coordinator,希望把它阻塞队列中的Reduce任务给抹掉
+		SendFinishMessageToCoordinator(job)
+	}
+}
+
+func SendFinishMessageToCoordinator(job *Job) {
+	// fmt.Printf("I finished a [%s] task, Jobid is [%d].\n", JobtypeToJobname[job.Jobtype], job.Jobid)
+	call("Coordinator.ReceiveFinishMessage", job, nil)
+}
+
+func ProcessMapTask(job *Job, mapf func(string, string) []KeyValue) {
+	// 参考mrsequential.go的代码
+	filepath := job.Filename
+	file, err := os.Open(filepath)
+	// intermediate 是 .txt 文件所有单词的统计
+	// 形式如 a:1 b:1 c:1 a:1 d:1 e:1 f:1
+	intermediate := []KeyValue{}
+	// fileInfo, err := file.Stat()
+	// fmt.Printf("Open the file: [%s] file size is [%d]\n", filepath, fileInfo.Size())
+
+	if err != nil {
+		log.Fatalf("cannot open %v", filepath)
+	}
+	content, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Fatalf("cannot read %v", filepath)
+	}
+	file.Close()
+
+	kva := mapf(filepath, string(content))
+	intermediate = append(intermediate, kva...)
+
+	oname := "mr-out-" + strconv.Itoa(job.Jobid)
+	for i := 0; i < job.NReduce; i++ {
+		os.Remove(oname + "-" + strconv.Itoa(i))
+	}
+
+	kvas := make([][]KeyValue, job.NReduce)
+	for i := 0; i < job.NReduce; i++ {
+		kvas[i] = make([]KeyValue, 0)
+	}
+
+	for _, kv := range intermediate {
+		index := keyReduceIndex(kv.Key, job.NReduce)
+		kvas[index] = append(kvas[index], kv)
+	}
+
+	for i := 0; i < job.NReduce; i++ {
+		sort.Slice(kvas[i], func(j, k int) bool {
+			return kvas[i][j].Key < kvas[i][k].Key
+		})
+	}
+
+	for i := 0; i < job.NReduce; i++ {
+		// kvas[i] 写入文件
+		filetemp, errtemp := os.Create(oname + "-" + strconv.Itoa(i))
+
+		if errtemp != nil {
+			log.Fatalf("cannot open %v", oname+"-"+strconv.Itoa(i))
+		}
+
+		// fmt.Printf("Successfully open the file: %s\n", oname + "-" + strconv.Itoa(i))
+
+		enc := json.NewEncoder(filetemp)
+		for _, kv := range kvas[i] {
+			err = enc.Encode(&kv)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+
+		filetemp.Close()
+	}
+
+}
+
+func keyReduceIndex(key string, nReduce int) int {
+	return ihash(key) % nReduce
+}
+
+func ProcessReduceTask(job *Job, reducef func(string, []string) string) {
+	// mr-out-X-job.Jobid,   目标文件 mr-out-job.Jobid
+	goal_name := "mr-out-" + strconv.Itoa(job.Jobid)
+	os.Remove(goal_name)
+
+	//需要得到文件的个数
+	intermediate := make([]KeyValue, 0)
+	for i := 1; i <= job.Filenum; i++ {
+		filetmpname := "mr-out-" + strconv.Itoa(i) + "-" + strconv.Itoa(job.Jobid)
+		filetmp, err := os.Open(filetmpname)
+		if err != nil {
+			log.Fatalf("cannot read %v", filetmpname)
+		}
+		dec := json.NewDecoder(filetmp)
+		for {
+			var kv KeyValue
+			if err := dec.Decode(&kv); err != nil {
+				break
+			}
+			intermediate = append(intermediate, kv)
+		}
+		filetmp.Close()
+	}
+	sort.SliceStable(intermediate, func(i, j int) bool {
+		return intermediate[i].Key < intermediate[j].Key
+	})
+
+	//开始写入
+	goal_file, err := os.Create(goal_name)
+	if err != nil {
+		log.Fatalf("Reduce task create %v file failed", goal_name)
+	}
+	i := 0
+	for i < len(intermediate) {
+		j := i + 1
+		for j < len(intermediate) && intermediate[j].Key == intermediate[i].Key {
+			j++
+		}
+		values := []string{}
+		for k := i; k < j; k++ {
+			values = append(values, intermediate[k].Value)
+		}
+		output := reducef(intermediate[i].Key, values)
+
+		// this is the correct format for each line of Reduce output.
+		fmt.Fprintf(goal_file, "%v %v\n", intermediate[i].Key, output)
+
+		i = j
+	}
+
+}
+
 // example function to show how to make an RPC call to the coordinator.
 //
 // the RPC argument and reply types are defined in rpc.go.
-//
 func CallExample() {
 
 	// declare an argument structure.
@@ -67,15 +239,15 @@ func CallExample() {
 	}
 }
 
-//
 // send an RPC request to the coordinator, wait for the response.
 // usually returns true.
 // returns false if something goes wrong.
-//
 func call(rpcname string, args interface{}, reply interface{}) bool {
 	// c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
 	sockname := coordinatorSock()
+	// Unix 域套接字是一种在同一台计算机上的进程之间进行通信的机制。
 	c, err := rpc.DialHTTP("unix", sockname)
+
 	if err != nil {
 		log.Fatal("dialing:", err)
 	}
